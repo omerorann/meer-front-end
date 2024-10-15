8de0079f6e814df57f25661e39f979edbbbb9914
omer <151935309+omerorann@users.noreply.github.com>
2024-10-15 21:55:40 +0300

    x

--- app/layout.js
+++ app/layout.js
@@ -3,7 +3,7 @@ import { Quicksand } from "next/font/google";
 import "./globals.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import StoreProvider from  "../redux/StoreProvider";
+import StoreProvider from "../redux/storeProvider";
 
 const quicksand = Quicksand({
   weight: ["400", "500", "700"],
--- redux/storeProvider.jsx
+++ redux/storeProvider.jsx
@@ -1,7 +1,7 @@
 // redux/StoreProvider.js
 "use client";
 import { Provider } from 'react-redux';
-import store from '../redux/store';
+import store from './store';
 
 const StoreProvider = ({ children }) => {
   return <Provider store={store}>{children}</Provider>;
